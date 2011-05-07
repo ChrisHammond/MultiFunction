530d6e775632445589ba42f92b292bad15e14351
christoc_cp <SND\christoc_cp>
2011-05-07 08:03:53 +0000

    first checkin of the MultiFunction skin for DotNetNuke. This is not completed and not packaged in any way yet

---
+++ Home.ascx.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Com.Christoc.MultiFunction
+{
+    public partial class Home : DotNetNuke.UI.Skins.Skin
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+          
+        }
+    }
+}
